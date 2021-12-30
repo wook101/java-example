7b8c0a1d287892c4a0044fc8ee0ffe25c5bdab17
wook101 <ehddnr1022@gmail.com>
2021-12-30 23:55:18 +0900

    ConstantStrignPool예제

---
+++ src/ConstantStringPoolEx.java
@@ -0,0 +1,10 @@
+public class ConstantStringPoolEx {
+
+
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = new String("def");
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
